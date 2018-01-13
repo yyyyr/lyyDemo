a2b27175f7eeb44a781b56e8cdb5662f76ecc166
miracle <18410226246@163.com>
2018-01-13 15:44:16 +0800

    888888

--- app/src/main/java/com/lyy/selfdefinedviewdemo/MainActivity.java
+++ app/src/main/java/com/lyy/selfdefinedviewdemo/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * 我有只小泰迪，她又两只小兔子
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
